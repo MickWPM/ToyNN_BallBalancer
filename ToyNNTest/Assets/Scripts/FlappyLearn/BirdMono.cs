d9804cfcb958e7bc67ea246924f64c8b2471bad7
Mick M <mickwpmcdonnell@gmail.com>
2019-02-20 16:35:48 +1100

    Refactored game logic to single bird game manager
    Added bird visualisation

--- ToyNNTest/Assets/Scripts/FlappyLearn/BirdMono.cs
+++ ToyNNTest/Assets/Scripts/FlappyLearn/BirdMono.cs
@@ -7,49 +7,29 @@ namespace FlappyLearn
 
     public class BirdMono : MonoBehaviour
     {
-        public float startHeight = 5f;
-        public float flapForce = 5f;
-        public BirdManager birdController;
-
-        Pillar pillar;
-        Queue<Pillar> pillars;
+        public bool useHuman = true;
         Queue<PillarMono> pillarGraphics;
 
-        public float pillarStartLoc = 40;
-        public float pillarEndLoc = -10;
-        public int numPillars = 5;
-
-        public float minGapMidpoint = 3;
-        public float maxGapMidpoint = 17;
-
         float deltaTime;
+        FlappyGame flappyGame;
+
         void Start()
         {
-            birdController = new BirdManager(new Bird(startHeight), flapForce, new RandomController());
-
-            pillars = new Queue<Pillar>();
-            pillarGraphics = new Queue<PillarMono>();
-
-            float pillarSpacing = (pillarStartLoc - pillarEndLoc) / numPillars;
-            for (int i = 0; i < numPillars; i++)
+            if (useHuman)
             {
-                float xPos = pillarSpacing * i + pillarStartLoc;
-                Pillar p = new Pillar(xPos, Random.Range(minGapMidpoint, maxGapMidpoint));
-                pillars.Enqueue(p);
-
-                PillarMono pillarShow = (PillarMono)Instantiate(pillarMonoPrefab);
-                pillarGraphics.Enqueue(pillarShow);
-                pillarShow.ShowPillar(p);
+                flappyGame = new FlappyGame(new HumanController());
+            } else
+            {
+                flappyGame = new FlappyGame(new RandomController());
             }
 
-            
+            flappyGame.OnGameEndedEvent += GameFinished;
         }
 
-        float gameTime;
-        void StartGame()
+        public void GameFinished(float score)
         {
-            gameStarted = true;
-            gameTime = 0;
+            gameStarted = false;
+            Debug.Log("SCORE: " + score);
         }
 
         public Transform birdDemo;
@@ -59,65 +39,48 @@ namespace FlappyLearn
         void Update()
         {
             if (!gameStarted)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Debug.Log("GAME STARTING");
+                    flappyGame.StartNewGame();
+                    gameStarted = true;
+                }
                 return;
-            deltaTime = Time.deltaTime;
+            }
+
+
+            flappyGame.Tick(Time.deltaTime);
             UpdateBirdMovement();
             UpdatePillarMovement();
-            CheckDeath();
-
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                birdController.Reset();
-            }
         }
 
 
         void UpdateBirdMovement()
         {
-            birdController.Tick(deltaTime, pillars.ToArray());
-            birdDemo.position = new Vector3(0, birdController.GetBird(0).Height, 0);
+            birdDemo.position = new Vector3(0, flappyGame.BirdHeight(), 0);
         }
 
-        float movementPerSecond = 5;
         void UpdatePillarMovement()
         {
-            foreach (Pillar p in pillars)
-            {
-                p.MovePillar(movementPerSecond * deltaTime);
-            }
+            ////foreach (Pillar p in pillars)
+            ////{
+            ////    p.MovePillar(movementPerSecond * deltaTime);
+            ////}
 
-            Pillar firstPillar = pillars.Peek();
+            ////Pillar firstPillar = pillars.Peek();
 
-            if (firstPillar.CentrePointX < pillarEndLoc)
-            {
-                pillars.Dequeue();
-                PillarMono pillarMono = pillarGraphics.Dequeue();
+            ////if (firstPillar.CentrePointX < pillarEndLoc)
+            ////{
+            ////    pillars.Dequeue();
+            ////    PillarMono pillarMono = pillarGraphics.Dequeue();
 
-                Pillar p = new Pillar(pillarStartLoc, Random.Range(minGapMidpoint, maxGapMidpoint));
-                pillarMono.ShowPillar(p);
+            ////    Pillar p = new Pillar(pillarStartLoc, Random.Range(minGapMidpoint, maxGapMidpoint));
+            ////    pillarMono.ShowPillar(p);
 
-                pillars.Enqueue(p);
-                pillarGraphics.Enqueue(pillarMono);
-            }
-
-
-        }
-
-
-        void CheckDeath()
-        {
-            foreach (Bird bird in birdController.birds)
-            {
-                foreach (var pillar in pillars)
-                {
-                    if (pillar.HitBird
-                        (bird))
-                    {
-                        bird.Die();
-                        break;
-                    }
-                }
-            }
+            ////    pillars.Enqueue(p);
+            ////    pillarGraphics.Enqueue(pillarMono);
+            ////}
         }
 
 
--- ToyNNTest/Assets/Scripts/FlappyLearn/FlappyGame.cs
+++ ToyNNTest/Assets/Scripts/FlappyLearn/FlappyGame.cs
@@ -8,8 +8,157 @@ namespace FlappyLearn
     public class FlappyGame
     {
 
+        public static float gravity = -50f;
+        public static float baseDeltaTime = 0.01f;
+        public static float birdStartHeight = 10;
+        public static float flapForce = 3000;
+
+        public static float pillarStartLoc = 40;
+        public static float pillarEndLoc = -10;
+        public static float numPillars = 5;
+        public static float minPillarGapMidpoint = 3;
+        public static float maxPillarGapMidpoint = 17;
 
 
+        Pillar[] pillarsArray;
+        Queue<Pillar> pillars;
+        float gameTime;
+        Bird bird;
+        IBirdController controller;
+        bool gameRunning = false;
+
+        public event System.Action<float> OnGameEndedEvent;
+
+        public FlappyGame(IBirdController controller, bool autostart = false)
+        {
+            bird = new Bird(birdStartHeight);
+            this.controller = controller;
+
+            if (autostart)
+            {
+                StartNewGame();
+            }
+        }
+
+        public void StartNewGame()
+        {
+            gameTime = 0;
+            bird = new Bird( birdStartHeight );
+            pillars = new Queue<Pillar>();
+            float pillarSpacing = (pillarStartLoc - pillarEndLoc) / numPillars;
+            for (int i = 0; i < numPillars; i++)
+            {
+                float xPos = pillarSpacing * i + pillarStartLoc;
+                Pillar p = new Pillar(xPos, Random.Range(minPillarGapMidpoint, maxPillarGapMidpoint));
+                pillars.Enqueue(p);
+            }
+            gameRunning = true;
+        }
+
+        public float BirdHeight()
+        {
+            return bird.Height;
+        }
+
+        public Pillar[] GetPillars()
+        {
+            return pillarsArray;
+        }
+
+        //Returns true if game is still running
+        float deltaTime;
+        float movementPerSecond = 5;
+        public bool Tick(float deltaTime)
+        {
+            if (gameRunning == false)
+                return false;
+
+            this.deltaTime = deltaTime;
+            gameTime += movementPerSecond * deltaTime;
+
+            UpdatePillarMovement();
+            pillarsArray = pillars.ToArray();
+
+            UpdateBirdMovement();
+
+            return !CheckDeath();
+        }
+
+        public bool Tick()
+        {
+            return Tick(baseDeltaTime);
+        }
+
+        FlappyGameState GetGameStateForBird()
+        {
+            FlappyGameState gameState = new FlappyGameState();
+            gameState.distToPipe = distToNearestPillar;
+            gameState.heightAboveMidPoint = bird.Height - nearestPillar.CentrePointGap;
+
+            return gameState;
+        }
+
+
+        float distToNearestPillar;
+        Pillar nearestPillar;
+        public void UpdateBirdMovement()
+        {
+            nearestPillar = (pillarsArray[0].CentrePointX + Pillar.halfWidth) > 0 ? pillarsArray[0] : pillarsArray[1];
+            distToNearestPillar = Mathf.Max(nearestPillar.CentrePointX - Pillar.halfWidth, float.Epsilon);
+
+
+            bird.ApplyForceOverTime(gravity, deltaTime);
+
+            if (controller.DoFlapFromState(GetGameStateForBird()))
+            {
+                bird.ApplyForceOverTime(flapForce, deltaTime);
+            }
+
+        }
+
+
+        void UpdatePillarMovement()
+        {
+            foreach (Pillar p in pillars)
+            {
+                p.MovePillar(movementPerSecond * deltaTime);
+            }
+
+            Pillar firstPillar = pillars.Peek();
+            if (firstPillar.CentrePointX < pillarEndLoc)
+            {
+                pillars.Dequeue();
+                Pillar p = new Pillar(pillarStartLoc, Random.Range(minPillarGapMidpoint, maxPillarGapMidpoint));
+                pillars.Enqueue(p);
+            }
+        }
+
+
+        bool CheckDeath()
+        {
+
+            if (bird.Height < 0)
+            {
+                BirdDie();
+                return true;
+            }
+
+            foreach (var pillar in pillars)
+            {
+                if (pillar.HitBird(bird))
+                {
+                    BirdDie();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void BirdDie()
+        {
+            bird.Die();
+            OnGameEndedEvent?.Invoke(gameTime);
+        }
 
     }
 
